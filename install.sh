56acdcaac23373c2c3133caa1842fa0db325bd28
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2016-03-10 19:29:09 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,5 +14,23 @@ ssh-keygen -t rsa
 #moves the .ssh to authorized_keys
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys 
 
-
+#give read and write permissions for the generated key value pairs
 chmod 0600 ~/.ssh/authorized_keys 
+
+#check to make sure Java is installed I have java 1.7.0
+java -version
+
+
+#move the tar file to the root /usr/include
+mv /home/ryan/Downloads/hadoop-2.6.0.tar.gz /usr/local
+
+#untar the file
+ tar xzf hadoop-2.6.0.tar.gz
+
+#rename to hadoop
+mkdir hadoop
+mv hadoop-2.6.0/* to hadoop/
+
+#exit the superuser and change to hadoop2 user
+exit
+su hadoop2
