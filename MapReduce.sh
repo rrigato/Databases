32091defbdbff471199e3af2828f44b121a15a22
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2016-03-15 10:45:23 -0700

    Update MapReduce.sh
    Have to add a hadoop environment variable to tell hadoop where the javac compiler is

--- MapReduce.sh
+++ MapReduce.sh
@@ -15,5 +15,122 @@ vim sample.txt
 #add the created file to hdfs
 hadoop fs -mkdir /user/hadoop2/input/
 
+#add the file from the local directory to hdfs
+ hadoop fs -put /home/hadoop2/sample.txt /user/hadoop2/input/
+
+#verify that the file is in hdfs
+hadoop fs -ls input
+
+
+
+#mkdir units
 
 #This is the Java file that will contain the MapReduce class
+vim ProcessUnits.java
+
+
+
+#MapReduce Example start
+
+
+package hadoop; 
+
+import java.util.*; 
+
+import java.io.IOException; 
+import java.io.IOException; 
+
+import org.apache.hadoop.fs.Path; 
+import org.apache.hadoop.conf.*; 
+import org.apache.hadoop.io.*; 
+import org.apache.hadoop.mapred.*; 
+import org.apache.hadoop.util.*; 
+
+public class ProcessUnits 
+{ 
+   //Mapper class 
+   public static class E_EMapper extends MapReduceBase implements 
+   Mapper<LongWritable ,/*Input key Type */ 
+   Text,                /*Input value Type*/ 
+   Text,                /*Output key Type*/ 
+   IntWritable>        /*Output value Type*/ 
+   { 
+      
+      //Map function 
+      public void map(LongWritable key, Text value, 
+      OutputCollector<Text, IntWritable> output,   
+      Reporter reporter) throws IOException 
+      { 
+         String line = value.toString(); 
+         String lasttoken = null; 
+         StringTokenizer s = new StringTokenizer(line,"\t"); 
+         String year = s.nextToken(); 
+         
+         while(s.hasMoreTokens())
+            {
+               lasttoken=s.nextToken();
+            } 
+            
+         int avgprice = Integer.parseInt(lasttoken); 
+         output.collect(new Text(year), new IntWritable(avgprice)); 
+      } 
+   } 
+   
+   
+   //Reducer class 
+   public static class E_EReduce extends MapReduceBase implements 
+   Reducer< Text, IntWritable, Text, IntWritable > 
+   {  
+   
+      //Reduce function 
+      public void reduce( Text key, Iterator <IntWritable> values, 
+         OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException 
+         { 
+            int maxavg=30; 
+            int val=Integer.MIN_VALUE; 
+            
+            while (values.hasNext()) 
+            { 
+               if((val=values.next().get())>maxavg) 
+               { 
+                  output.collect(key, new IntWritable(val)); 
+               } 
+            } 
+ 
+         } 
+   }  
+   
+   
+   //Main function 
+   public static void main(String args[])throws Exception 
+   { 
+      JobConf conf = new JobConf(ProcessUnits.class); 
+      
+      conf.setJobName("max_eletricityunits"); 
+      conf.setOutputKeyClass(Text.class);
+      conf.setOutputValueClass(IntWritable.class); 
+      conf.setMapperClass(E_EMapper.class); 
+      conf.setCombinerClass(E_EReduce.class); 
+      conf.setReducerClass(E_EReduce.class); 
+      conf.setInputFormat(TextInputFormat.class); 
+      conf.setOutputFormat(TextOutputFormat.class); 
+      
+      FileInputFormat.setInputPaths(conf, new Path(args[0])); 
+      FileOutputFormat.setOutputPath(conf, new Path(args[1])); 
+      
+      JobClient.runJob(conf); 
+   } 
+} 
+
+#MapReduce Sample End
+
+
+
+#check to make sure java compiler is intalled
+javac -version
+
+#add to the .bashrc file
+#have to make a global variable so that hadoop knows where the javac
+#compiler is
+vim .bashrc
+export HADOOP_CLASSPATH=$JAVA_HOME/lib/tools.jar
