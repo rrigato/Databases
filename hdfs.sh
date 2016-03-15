b66959dd792196bcdf49f2c633beacafa2b68703
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2016-03-14 17:15:54 -0700

    Update hdfs.sh
    Added a few more examples of using hdfs from the command line

--- hdfs.sh
+++ hdfs.sh
@@ -21,3 +21,19 @@ hadoop fs -ls /user/hadoop2/input3
 
 #getting the memory used from different directories
 hadoop fs -du  /user/hadoop2/ /user/hadoop2/input3/myout.txt
+
+#view the file in the hdfs
+hadoop fs -cat /user/hadoop2/input3/myout.txt
+
+## hadoop fs -get <hdfs_src> <localdst> 
+#get a file from the hdfs
+hadoop fs -get /user/hadoop2/input3/myout.txt /home/hadoop2/Documents/
+
+hadoop fs -put file.txt /user/hadoop2/input3/file.txt
+
+hadoop fs -mkdir /user/hadoop2/input2
+
+#copy directories to a different directory
+#places input3 into input2
+# hadoop distcp <src-url> <dest-url>
+hadoop distcp /user/hadoop2/input3/ /user/hadoop2/input2/
